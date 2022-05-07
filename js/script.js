f678e7b8940187915b80f08e4067d8f2262d90eb
Wadim Martinkevich <martinkewichwadim@gmail.com>
2022-05-07 16:02:25 +0300

    feat: update keyboard

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const keyboard = document.querySelector(".keyboard");
 const colorsInput = document.querySelector(".colors__input");
 const textField = document.querySelector(".text");
 const letters = document.querySelectorAll(".keyboard__button span");
-const wrongKeys = ["Shift1", "Shift2", "Control1", "Control2", "Alt1", "Alt2", "Backspace0", "Tab0", "Enter0","CapsLock0"];
+const wrongKeys = ["Shift1", "Shift2", "Control1", "Control2", "Alt1", "Alt2", "Backspace0", "Tab0", "Enter0","CapsLock0","ArrowLeft0","ArrowRight0","ArrowDown0","ArrowUp0"];
 
 nightMode.addEventListener("click", () => {
     nightMode.classList.toggle("active");
@@ -16,24 +16,36 @@ nightMode.addEventListener("click", () => {
         button.classList.toggle("night");
     })
 });
-
+keyboard.addEventListener("click",(e)=>{
+    if(e.target.classList.contains('keyboard__button-main')){
+        textField.value += e.target.innerText
+    }
+    if(e.target.classList.contains('keyboard__button-backpace')){
+        textField.value = textField.value.slice(0, -1)
+    }
+    if(e.target.classList.contains('keyboard__button-space')){
+        textField.value += " "
+    }
+    if(e.target.classList.contains('keyboard__button-enter')){
+        textField.value += "\n"
+    }
+    console.log(e.target)
+    
+})
 keyboardButtons.forEach((button) => {
-    button.addEventListener("mousedown", () => {
+    button.addEventListener("click", () => {
         button.classList.add("active");
         setTimeout(() => {
             if (button.classList.contains("active")) {
                 button.classList.remove("active");
             }
-        }, 1000)
+        }, 300)
     });
     button.addEventListener("mouseup", () => {
         button.classList.remove("active");
     });
 });
 colorsInput.addEventListener("input", () => {
-    // for (let i = 0; i < keys.length; i++) {
-    //     keys[i].style.color = colorsInput.value;
-    // }
     keyboard.style.background = colorsInput.value;
 });
 
@@ -68,7 +80,9 @@ window.addEventListener('keyup', function (e) {
     if(name == 'CapsLock0' ){
        keys[name].active = !keys[name].active;
     }
+   
     else  keys[name] = false;
+    console.log(name);
     
     switch (keys[e.key + e.location]) {
         case false: if (keys.Shift1 == true && keys.CapsLock0.active == false ) { textField.value += e.key.toUpperCase() }
@@ -81,6 +95,9 @@ window.addEventListener('keyup', function (e) {
         }
         else if (name == "Backspace0") {
             textField.value = textField.value.slice(0, -1)
+        }
+        else if (name == "Enter0") {
+            textField.value += "\n";
         }
 
             break;
